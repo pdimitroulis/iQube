dd0c13ae7236bb7b71a8c61bf40a5cf086608720
Pantelis Dimitroulis <padimitroulis@gmail.com>
2020-02-08 19:52:44 -0600

    Update MainMenu.cs

--- MainMenu.cs
+++ MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
 	public void PlayGame () {
-		SceneManager.LoadScene (1); //TODO: load latest unlocked scene
+		SceneManager.LoadScene (1);
 	}
 
 	public void QuitGame () {
